f96959b9280ef0b7884923a1b038011eec45b68a
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-04-28 21:08:45 -0300

    otro

--- src/datos.ts
+++ src/datos.ts
@@ -4,6 +4,6 @@ let rotulo = document.getElementById("rotulo");
 rotulo.innerHTML = "Ingrese un dato:";
 btnEnv.addEventListener("click", () => {
   // TU CODIGO VA AQUI, POR EJEMPLO:
-  console.log("el dato ingresado es ", dato.value);
+  console.log("el dato ingresado es: ", dato.value);
   //}
 });
