ac554a816aad4c100feaff9d5039b4c926613e56
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-04-29 10:10:27 -0300

    pruebas

--- src/datos.ts
+++ src/datos.ts
@@ -1,12 +1,12 @@
-let btnEnv = document.getElementById("btnEnviar");
+/*let btnEnv = document.getElementById("btnEnviar");
 let dato = document.getElementById("dato1");
 let rotulo = document.getElementById("rotulo");
 rotulo1.innerHTML = "Base:";
 rotulo2.innerHTML = "Altura:";
-//btnEnv.addEventListener("click", () => {
+btnEnv.addEventListener("click", () => {
   // TU CODIGO VA AQUI, POR EJEMPLO:
   console.log("el dato ingresado es: ", dato.value);
   //}
 });
 
-//<button id="btnEnviar">Enviar</button>
+<button id="btnEnviar">Enviar</button>; */
--- src/ejercicio.ts
+++ src/ejercicio.ts
@@ -1,7 +1,19 @@
+let btnEnv = document.getElementById("btnEnv");
 let dato1 = document.getElementById("dato1");
 let dato2 = document.getElementById("dato2");
+rotulo1.innerHTML = "Ingrese la base: ";
+rotulo2.innerHTML = "Ingrese la altura: ";
 
 let base: number = Number(dato1.value);
 let altura: number = Number(dato2.value);
+let area: number = base * altura;
 
-let area: number =
+//btnEnviarBase.addEventListener("click", () => {
+//console.log("La base es:", dato1.value);
+//});
+
+//btnEnviarAltura.addEventListener("click", () => {
+//console.log("La altura es:", dato2.value);
+btnEnv.addEventListener("click", () => {
+  console.log("Resultado:", area);
+});
