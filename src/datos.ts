2699707485794591b14269c8cda0e85790e6c18e
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-04-28 22:20:43 -0300

    ejercicio nuevo

--- src/datos.ts
+++ src/datos.ts
@@ -1,9 +1,12 @@
 let btnEnv = document.getElementById("btnEnviar");
-let dato = document.getElementById("dato");
+let dato = document.getElementById("dato1");
 let rotulo = document.getElementById("rotulo");
-rotulo.innerHTML = "Ingrese un dato:";
-btnEnv.addEventListener("click", () => {
+rotulo1.innerHTML = "Base:";
+rotulo2.innerHTML = "Altura:";
+//btnEnv.addEventListener("click", () => {
   // TU CODIGO VA AQUI, POR EJEMPLO:
   console.log("el dato ingresado es: ", dato.value);
   //}
 });
+
+//<button id="btnEnviar">Enviar</button>
---
+++ src/ejercicio.ts
@@ -0,0 +1,7 @@
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+
+let base: number = Number(dato1.value);
+let altura: number = Number(dato2.value);
+
+let area: number =
